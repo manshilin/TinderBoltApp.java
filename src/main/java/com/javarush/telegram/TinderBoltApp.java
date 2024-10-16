8d3fa0e25ab54ffcf244659e1e08b36db733edea
Sergii Manshylin <sergeymanshilin@gmail.com>
2024-10-16 10:47:58 +0300

    tinder bot gpt

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -14,7 +14,10 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class TinderBoltApp extends MultiSessionTelegramBot {
     public static final String TELEGRAM_BOT_NAME = "my_telegramtinder_bot"; 
     public static final String TELEGRAM_BOT_TOKEN = "8138281864:AAE9qULm0sisyF2Ojbs-3PdNFgA22ollFN8"; 
-    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; 
+    public static final String OPEN_AI_TOKEN = "gpt:"; 
+    public DialogMode mode = DialogMode.MAIN;
+    public ChatGPTService gptService = new ChatGPTService(OPEN_AI_TOKEN);
+
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
     }
@@ -24,18 +27,42 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
         String message = getMessageText();
 
         if (message.equals("/start")) {
-            sendTextMessage("*HELLO!*");
+            mode = DialogMode.MAIN;
+            showMainMenu(
+                "Main menu", "/start",
+                "генерація Tinder-профілю 😎", "/profile",
+                
+                 "повідомлення для знайомства 🥰", "/opener",
+                "листування від вашого імені 😈", "/message",
+                "листування із зірками 🔥", "/date",
+                "поставити запитання чату GPT 🧠", "/gpt"
+
+            );
+            String menu = loadMessage("main");
+            sendTextMessage(menu);
             sendPhotoMessage("main");
 
             return;
         }
         
-        sendTextMessage("_" + message + "_");
-        sendTextButtonsMessage("Button message",
-        "START", "start",
-        "STOP", "stop"); 
-    }
+        if (message.equals("/gpt")) {
+            mode = DialogMode.GPT;
 
+            String gptMessage = loadMessage("gpt");
+            sendTextMessage(gptMessage);
+            sendPhotoMessage("gpt");
+
+            return;
+        }
+        if (mode == DialogMode.GPT) {
+            String promt = loadPrompt("gpt");
+            String answer = gptService.sendMessage(promt, message);
+            sendTextMessage(answer);
+            return;
+
+        }
+        
+    }
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         telegramBotsApi.registerBot(new TinderBoltApp());
